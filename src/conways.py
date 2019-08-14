feaf889cc53479bf5be4e471ff3f722f6049fd4f
Jason Hedrick <jasonleehedrick@gmail.com>
2019-08-13 19:54:40 -0700

    Have it sort of working with the one dimensional array. Time to work on a matrix.

--- src/conways.py
+++ src/conways.py
@@ -7,9 +7,11 @@ GRAY = (25, 25, 25)
 WIN_SIZE = 500
 
 cur_states = [0] * 400
+cur_states[9] = 1
 cur_states[10] = 1
+cur_states[29] = 1
 cur_states[30] = 1
-cur_states[50] = 1
+
 next_states = []
 
 pygame.init()
@@ -28,6 +30,78 @@ done = False
 # Used to manage how fast the screen updates
 clock = pygame.time.Clock()
  
+
+# Conway GOL Update
+def getNumNeighbors(cur_index):
+    '''
+    We need to check....
+    The grid is setup as a one dimensional array.
+    With the current state of the code it calculates in the Y direction before looping back to the top of the screen.
+    ____|||||||||||||| Indexes
+        0, 20, 40, 60, 80, 100, 120, 140
+        1, 21,
+        2, 22,
+        3
+        4
+        5
+        6
+        7
+        8
+        9,  29, 49,
+        10, 30, 50, 70, 90, 110, 130, 150 ->
+        11, 31, 51,
+        12
+        13
+        14
+        15
+        16
+        17
+        18
+        19
+
+        cur_index-1 is North
+        cur_index+1 is South
+        cur_index-20 is West
+        cur_index+20 is East
+        cur_index-21 is Northwest
+        cur_index+19 is Northeast
+        cur_index-19 is Southwest
+        cur_index+21 is Southeast
+        
+    '''
+    # Get the 'cardinal directions' of the neighbors based off our cur_index
+    neighbors = 0
+    cardinalDirections = {
+        'north': -1,
+        'south': -1,
+        'west': -20,
+        'east': 20,
+        'nw': -21,
+        'ne': 19,
+        'sw': -19,
+        'se': 21
+    }
+    
+    for direction in cardinalDirections:
+        if cur_index - cardinalDirections[direction] >= 0 and cur_index - cardinalDirections[direction] <= len(cur_states)-1:
+            if cur_states[cur_index - cardinalDirections[direction]] == 1:
+                neighbors += 1
+
+    return neighbors
+
+def updateState():
+    copy_states = cur_states[:]
+    for cur_index in range(len(cur_states)):
+        #print(f'neighbors for cell {cur_index} : {getNumNeighbors(cur_index)}')
+        neighbors = getNumNeighbors(cur_index)
+        if neighbors == 3:
+            # Birth
+            copy_states[cur_index] = 1
+        elif neighbors == 1 or neighbors >= 4:
+            # Death
+            copy_states[cur_index] = 0
+    return copy_states
+
 # -------- Main Program Loop -----------
 while not done:
     # --- Main event loop
@@ -35,10 +109,10 @@ while not done:
         if event.type == pygame.QUIT:
             done = True
  
+    print(getNumNeighbors(30))
     # --- Game logic should go here
-    
+    cur_states = updateState()
 
- 
     # --- Screen-clearing code goes here
  
     # Here, we clear the screen to gray. Don't put other drawing commands
@@ -51,6 +125,7 @@ while not done:
     while x < WIN_SIZE:
         y = 5
         while y < WIN_SIZE:
+            
             state = cur_states[cur_index]
             if state == 0:
                 pygame.draw.rect(screen, BLACK, pygame.Rect(x, y, 20, 20))
